ae790fc3a3663e9f59d24e7f0af8791f556a0e80
Melissa Evangelista <melissaevangelista52@gmail.com>
2023-04-18 23:17:00 -0400

    finished all tasks

--- index.js
+++ index.js
@@ -234,10 +234,22 @@ let cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
  
 class CuboidMakerTwo {
+  constructor ({length, width, height}) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
 
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+
+  volume() {
+    return this.length * this.width * this.height;
+  }
 }
 
-
+let cuboidTwo = new CuboidMakerTwo({length: 10, width: 20, height: 30})
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
